735464a5473927a9b8a94b027a54e9e0eeeb8cdc
Ben <ben@Bens-MacBook-Pro.local>
2024-12-05 21:43:23 -0500

    Day 2b challenge

--- challenges/02/code.py
+++ challenges/02/code.py
@@ -3,15 +3,38 @@ import pandas as pd
 with open('challenges/02/data.txt', 'r') as f:
     rows = [list(map(int, line.split())) for line in f]
 
+def is_safe(diff):
+    monotonic = ((diff[1:] > 0).all()) | ((diff[1:] < 0).all())
+    gradual = ((diff[1:].abs().between(1, 3, inclusive='both'))).all()
+    safe = monotonic * gradual
+    return safe
+
 n = 0
 
 for row in rows:
     s = pd.Series(row)
     diff = s.diff()
-    monotonic = ((diff[1:] > 0).all()) | ((diff[1:] < 0).all())
-    gradual = ((diff[1:].abs().between(1, 3, inclusive='both'))).all()
-    safe = monotonic * gradual
+    safe = is_safe(diff)
     if safe:
         n += 1
 
-print(n)
+print(f'Safe reports: {n}')
+
+n = 0
+
+for row in rows:
+    s = pd.Series(row)
+    diff = s.diff()
+    safe = is_safe(diff)
+    if safe:
+        n += 1
+    else:
+        for i in range(len(s)):
+            s_pd = s.drop(i)
+            diff_pd = s_pd.diff()
+            safe_pd = is_safe(diff_pd)
+            if safe_pd:
+                n += 1
+                break
+
+print(f'Safe reports with Problem Dampener: {n}')
